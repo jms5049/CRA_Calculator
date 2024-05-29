4a1001bc4458dd32a3fb8aa95bc1cc49fcbb7c57
min.byun <leaftotoro@gmail.com>
2024-05-29 16:23:42 +0900

    add unittest code

--- Sample-Test1/cal_test.cpp
+++ Sample-Test1/cal_test.cpp
@@ -1,6 +1,25 @@
 #include "pch.h"
+#include "../Calc/cal.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(Calculator, GetMinus1) {
+	const int a = 10;
+	const int b = 20;
+	int expect = a - b;
+	int actual = getMinus(a, b);
+
+	EXPECT_EQ(expect, actual);
+}
+
+TEST(Calculator, GetMinus2) {
+	const int a = 10;
+	const int b = 20;
+	int expect = a - b;
+	int actual = getMinus(a, b);
+
+	EXPECT_EQ(expect, actual);
 }
