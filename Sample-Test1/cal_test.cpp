032bb075699e6871bc5ee79a65f3c76e5cdf3904
ahran_.choi <carokq@gmail.com>
2024-05-29 16:12:55 +0900

    TC 생성. 잘 수행 되는 것 확인.

--- Sample-Test1/cal_test.cpp
+++ Sample-Test1/cal_test.cpp
@@ -1,6 +1,16 @@
 #include "pch.h"
+#include "../Calc/cal.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(Calculator, GetSum) {
+	const int a = 10;
+	const int b = 20;
+	int expect = a + b;
+	int actual = getSum(a, b);
+
+	EXPECT_EQ(expect, actual);
 }
